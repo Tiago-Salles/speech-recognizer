612b7bcb1b3e7e60d87fe9cc51b17cb324f53b37
Tiago <tiago.salles@houseasy.net>
2022-07-13 14:03:48 -0300

    Feat: Created recognition, first commit in the main

---
+++ main.py
@@ -0,0 +1,24 @@
+import speech_recognition as speechRecognition
+import pyttsx3 
+
+recognizer = speechRecognition.Recognizer()
+
+def recognize():
+    try:
+        with speechRecognition.Microphone() as source:
+            speaker = pyttsx3.init()
+            print("listening...")
+            audio = recognizer.listen(source)
+            command = recognizer.recognize_google(audio)
+            
+            for voice in speaker.getProperty("voices"):
+                if(voice.id == "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"):
+                    speaker.setProperty("voice", voice.id)
+            speaker.say(command)
+            print(command)
+            speaker.runAndWait()
+            
+    except:
+       pass
+
+recognize()
