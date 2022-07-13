5e646fb09716d43007d522e292405732ef3946b2
Tiago <tiago.salles@houseasy.net>
2022-07-13 14:04:12 -0300

    Just to push

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def recognize():
             print("listening...")
             audio = recognizer.listen(source)
             command = recognizer.recognize_google(audio)
-            
             for voice in speaker.getProperty("voices"):
                 if(voice.id == "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"):
                     speaker.setProperty("voice", voice.id)
