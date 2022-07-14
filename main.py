820d8ec2774e394bfef8c1959f7d0f90116bfa95
Tiago <tiago.salles@houseasy.net>
2022-07-14 07:45:11 -0300

    Feat: Created new commands

--- main.py
+++ main.py
@@ -37,8 +37,6 @@ def receiveCommands():
                 elif "GMAIL" in command:
                     speaker.say("Opening gmail")
                     webbrowser.open("https://mail.google.com/mail/u/0/")
-                elif "PIETRA" in command:
-                    speaker.say("I am here!")
                 elif "NEW APP" in command:
                     speaker.say("Opening Azure in V2")
                     webbrowser.open("https://dev.azure.com/houseasy/_git/HouseasyFlutterAppV2/branches")
@@ -48,14 +46,36 @@ def receiveCommands():
                 elif "ACTUAL APP" in command:
                     speaker.say("Opening Azure in actual app")
                     webbrowser.open("https://dev.azure.com/houseasy/_git/HouseasyFlutterApp")
-                elif "GATHER" in command:
+                elif "OFFICE" in command:
                     speaker.say("Opening the gather")
                     webbrowser.open("https://app.gather.town/app/qnYTErWAOgoz87Oi/antigoescritorio")
+                elif "PIANO" in command:
+                    speaker.say("Playing your piano album")
+                    webbrowser.open("https://youtu.be/Jn09UdSb3aA")
+                elif "GUITAR" in command:
+                    speaker.say("Playing your classic guitar playlist")
+                    webbrowser.open("https://www.youtube.com/watch?v=DsWKDfkEat8&list=RDDsWKDfkEat8&start_radio=1")
+                elif "GITHUB" in command:
+                    speaker.say("Opening your github")
+                    webbrowser.open("https://github.com/Tiago-Salles")
+                elif "TRANSLATOR PORTUGUESE" in command:
+                    speaker.say("Opening translator")
+                    webbrowser.open("https://translate.google.com.br/?hl=pt-BR&sl=pt&tl=en&op=translate")
+                elif "TRANSLATOR ENGLISH" in command:
+                    speaker.say("Opening translator")
+                    webbrowser.open("https://translate.google.com.br/?hl=pt-BR&sl=en&tl=pt&op=translate")
+                elif "GOOGLE" in command:
+                    speaker.say("Opening google")
+                    webbrowser.open("https://www.google.com/")
                 else:
                     speaker.say("Improve me, and i will know what to do")
                     speaker.runAndWait()
             elif "THANK YOU" in command or "THANKS" in command:
                 speaker.say("I am here for you!")
+            elif "SORRY" in command:
+                speaker.say("Dont worry")
+            elif "ARE YOU" in command or "EASY ARE YOU" in command:
+                speaker.say("I am aways here for you!")
             elif "FINISH" in command:
                     speaker.say("Good bye")
                     speaker.runAndWait()
